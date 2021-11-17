fda4907dda9b5043ba941a6a8cd48af4547152b6
ErikDuba <80678269+ErikDuba@users.noreply.github.com>
2021-11-17 17:44:16 +0100

    Is nu mogelijk om bij 'naam' én bij 'leeftijd' de loop te stoppen

---
+++ NamenEnLeeftijden.py
@@ -0,0 +1,13 @@
+def gegevens():
+    naam = input('Wat is uw naam?: ')
+    leeftijd = input('Wat is uw leeftijd?: ')
+i = 1
+while i == 1:
+    naam = input('Wat is uw naam?: ')
+    if naam == 'stop':
+        break
+    leeftijd = input('Wat is uw leeftijd?: ')
+    if leeftijd == 'stop':
+        break
+    else: 
+        print('Hallo ' + str(naam) + ' uw naam is ' + str(naam) + ' en uw leeftijd is ' + str(leeftijd))
