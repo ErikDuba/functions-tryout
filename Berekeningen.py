2f8415122667e6917249ac811111c8cb616d8f05
ErikDuba <80678269+ErikDuba@users.noreply.github.com>
2021-11-23 12:04:10 +0100

    het werkt nu oe het moet horen met 2 defs die elk 3 berekeningen doen

--- Berekeningen.py
+++ Berekeningen.py
@@ -1,10 +1,48 @@
-def kleineBerekening():
-    1 + 1 
-    1 + 2 
-    1 + 3 
-def groteBerekening():
-    2 + 1 
-    2 + 2
-    2 + 3 
+import time
+import os
+def clear():
+    command = 'cls'
+    os.system(command)
 
-print('10 + 12 = 22 \n58 - 34 = 24 \n6 x 7 = 42 \n144 : 12 = 12 \n12 + 1 = 13 \n34 - 1 = 33')
+clear()
+def calc1():
+    print('This will add both numbers together:')
+    number1 = int(input('What is number 1?: '))
+    number2 = int(input('What is number 2?: '))
+    outcome1 = number1 + number2
+
+    print('This will subtract your second number from the first:')
+    number3 = int(input('What is number 3?: '))
+    number4 = int(input('What is number 4?: '))
+    outcome2 = number3 - number4
+
+    print('This will multiply your numbers:')
+    number5 = int(input('What is number 5?: '))
+    number6 = int(input('What is number 6?: '))
+    outcome3 = number5 * number6
+    
+    print(str(number1) + ' + ' + str(number2) + ' = ' + str(outcome1)) 
+    print(str(number3) + ' - ' + str(number4) + ' = ' + str(outcome2))
+    print(str(number4) + ' * ' + str(number5) + ' = ' + str(outcome3))
+
+
+def calc2():
+    print('This will divide your first number by the second')
+    number7 = int(input('What is number 7?: '))
+    number8 = int(input('What is number 8?: '))
+    outcome4 = number7 / number8
+
+    print('This will add one to your number')
+    number9 = int(input('What is number 9?: '))
+    outcome5 = number9 + 1
+
+    print('This will subtract one from your number')
+    number10 = int(input('What is number 10?: '))
+    outcome6 = number10 - 1
+
+    print(str(number7) + ' : ' + str(number8) + ' = ' + str(outcome4))
+    print(str(number9) + ' + 1 = '  ' = ' + str(outcome5))
+    print(str(number10) + ' - 1 = ' + str(outcome6))
+
+calc1()
+calc2()
